30945bbda2eb2597a629fc28c17d5fbea8d0fcff
Joramcode47 <joram4798@gmail.com>
2025-10-12 00:49:03 -0600

    Se realiza modificación en el codigo de EjercicioCondicionales.py

--- EjercicioCondicionales.py
+++ EjercicioCondicionales.py
@@ -1,14 +1,21 @@
 
+'''
+    Desarrolla un Script que pida la edad y altura del usuario 
+    Si la persona es mayor de edad puede participar
+    Pero la persona debe medir mas de 1.70 
+    Si la persona no mide 1.70 pero es mayor a 25 años
+    y la altura es mayor a 1.65
+'''
+
+
 edad = int(input("Ingrese su edad: "))
 altura = float(input("Ingrese su altura: "))
 
 
 if 0 < edad < 100:
     if edad >= 18:
-        if altura > 1.70:
+        if (altura >= 1.70) or (edad >= 25 and altura > 1.65):
             print(f'La persona si puede participar ya que tiene {edad} años y mide {altura:.2f}.')
-        elif edad > 25 and altura > 1.65:
-            print(f'La persona si puede participar ya que mide {altura:.2f} pero tiene {edad} años.')
         else:
             print("no puede acceder debido a que no cumple con el requerimiento de edad y altura.")
     else:
