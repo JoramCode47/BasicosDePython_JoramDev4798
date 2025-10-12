8a49b7e61c47f8d42ad14703770c092a92fb3a4e
Joramcode47 <joram4798@gmail.com>
2025-10-11 22:56:07 -0600

    Se agrega ejemplo de condicion if else y sus variantes

--- CondicionalesPython.py
+++ CondicionalesPython.py
@@ -1,2 +1,14 @@
 # CONDICIONALES
 
+edad = 116
+
+if 0 < edad < 100:
+    print("Edad Correcta \n")
+    if edad >= 18:
+        print("Eres mayor de edad")
+    elif edad == 17:
+        print("Ya casi eres mayor de edad")
+    else:
+        print("Eres menor de edad")
+else:
+    print("Edad Incorrecta")
